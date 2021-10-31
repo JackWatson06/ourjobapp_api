fb3dd27444b0fce248ff27dad72aa4acd5130955
JackWatson06 <markchangan@gmail.com>
2021-10-31 19:22:28 -0400

    Commit for Jack

--- src/modules/connection/Seeding.js
+++ src/modules/connection/Seeding.js
@@ -1,8 +1,95 @@
 
+import * as c from "infa/Constants";
+
 
 const employer_1 = {
+    token_id      : null,
+    fname        : string
+    lname        : string
+    position     : string,
+    company_name : string,
+    verified     : boolean,
+    email        : string,
+
+    //I can't hear you on google meets anymore.
+    // I lost internet aparently. RIP can you still see my words?
+    
+    industry     : ObjectId[],
+    place_id     : string,   // <= Select multiple of the job group
+    experience   : number[],   // <= Experience level is select multiple
+    salary       : number,
+    commitment   : number,
+    where        : number,
+    authorized   : boolean,
 
 }
+
+
+const employer_2 = {
+
+}
+
+
+
+const employer_3 = {
+
+}
+
+
+
+
+
+
+const employee_1_matches_1 = {
+    token_id      : null,
+    fname         : "Employee 1",
+    lname         : "Employer 1",
+    information   : "Random",
+    email         : "email@gmail.com",
+    phone         : "000-000-0000",
+    verified      : 0000000,
+    education     : 1,
+
+
+    // nations       ?: ObjectId[],   // List of country IDs from the database.
+    // place_id      ?: string,       // 
+    // job_id        : ObjectId[],
+    // authorized    : ObjectId[],
+    hourly_rate   : 15,
+    commitment    : c.Commitment.FULL_TIME,
+    where         : c.Where.IN_PERSON,
+    distance      : c.Distance.TWO_HUNDRED_FIFTY_MILES,
+    experience    : c.Experience.ENTRY
+}
+
+const employee_2_matches_1 = {
+    
+}
+
+const employee_3_matches_1 = {
+    
+}
+
+const employee_4_matches_1 = {
+    
+}
+
+const employee_5_matches_1 = {
+    
+}
+
+const employee_6_matches_1 = {
+    
+}
+
+const employee_2_matches_1 = {
+    
+}
+
+const employee_2_matches_1 = {
+    
+}
+
 
 
 
